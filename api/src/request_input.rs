f7716d54dc8584f7ad9284d5dd5499e349ea0e34
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-06-30 18:56:05 +0530

    Update request_input.rs
    Implement URL validation with 'validate derive' and  'validate' attributes to ensure correct format

--- api/src/request_input.rs
+++ api/src/request_input.rs
@@ -1,7 +1,9 @@
 use serde::{Deserialize, Serialize};
+use validator::Validate;
 
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Validate)]
 pub struct CraeteWebsiteInput {
+    #[validate(url)]
     pub url: String
 }
